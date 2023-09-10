e054c33061748107743ca5516caceed58a2f45ba
cuishengfei <3306253852@qq.com>
2023-09-10 11:28:54 +0800

    merge two sorted lists v3

--- src/merge_two_sorted_lists.rs
+++ src/merge_two_sorted_lists.rs
@@ -67,6 +67,30 @@ pub fn merge_two_lists_v2(
   }
 }
 
+pub fn merge_two_lists_v3(
+  list1: Option<Box<ListNode>>,
+  list2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+  let mut list1 = list1;
+  let mut list2 = list2;
+  let mut prehead = ListNode::new(0);
+  let mut cur_node = &mut prehead;
+
+  while let (Some(node1), Some(node2)) = (&list1, &list2) {
+    if node1.val <= node2.val {
+      cur_node.next = list1.take();
+      cur_node = cur_node.next.as_mut().unwrap();
+      list1 = cur_node.next.take();
+    } else {
+      cur_node.next = list2.take();
+      cur_node = cur_node.next.as_mut().unwrap();
+      list2 = cur_node.next.take();
+    }
+  }
+  cur_node.next = list1.or(list2);
+  prehead.next
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -86,23 +110,15 @@ mod tests {
     n4.next = Some(Box::new(n5));
     println!(
       "{:?}",
-      merge_two_lists_v1(Some(Box::new(n1)), Some(Box::new(n4)))
+      merge_two_lists_v1(Some(Box::new(n1.clone())), Some(Box::new(n4.clone())))
     );
-    let mut n1 = ListNode::new(1);
-    let mut n2 = ListNode::new(3);
-    let mut n3 = ListNode::new(9);
-    let n7 = ListNode::new(9);
-    let mut n4 = ListNode::new(2);
-    let mut n5 = ListNode::new(4);
-    let n6 = ListNode::new(8);
-    n3.next = Some(Box::new(n7));
-    n2.next = Some(Box::new(n3));
-    n1.next = Some(Box::new(n2));
-    n5.next = Some(Box::new(n6));
-    n4.next = Some(Box::new(n5));
     println!(
       "{:?}",
-      merge_two_lists_v2(Some(Box::new(n1)), Some(Box::new(n4)))
+      merge_two_lists_v2(Some(Box::new(n1.clone())), Some(Box::new(n4.clone())))
+    );
+    println!(
+      "{:?}",
+      merge_two_lists_v3(Some(Box::new(n1.clone())), Some(Box::new(n4.clone())))
     );
   }
 }
